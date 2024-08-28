83f8195ef3bc4bd927b37aae57b222d8a8fe6b9d
John Doe <abdullahalmasrur8@gmail.com>
2024-08-29 00:32:20 +0600

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import React from "react";
 
 import AppLayout from "./ui/AppLayout.jsx";
@@ -16,24 +16,22 @@ import Settings from "./pages/Settings.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="homepage" />} />
-          <Route path="homepage" element={<Home />} />
-          <Route path="album" element={<Albums />} />
-          <Route path="artists" element={<Artists />} />
-          <Route path="folders" element={<Folders />} />
-          <Route path="genres" element={<Genres />} />
-          <Route path="mostPlayed" element={<MostPlayed />} />
-          <Route path="nowPlaying" element={<NowPlaying />} />
-          <Route path="playlists" element={<Playlists />} />
-          <Route path="queue" element={<Queue />} />
-          <Route path="library" element={<Library />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <Route element={<AppLayout />}>
+        <Route index element={<Navigate replace to="homepage" />} />
+        <Route path="homepage" element={<Home />} />
+        <Route path="album" element={<Albums />} />
+        <Route path="artists" element={<Artists />} />
+        <Route path="folders" element={<Folders />} />
+        <Route path="genres" element={<Genres />} />
+        <Route path="mostPlayed" element={<MostPlayed />} />
+        <Route path="nowPlaying" element={<NowPlaying />} />
+        <Route path="playlists" element={<Playlists />} />
+        <Route path="queue" element={<Queue />} />
+        <Route path="library" element={<Library />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+    </HashRouter>
   );
 }
 
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <p>AppLayout</p>
+      <Outlet />
+    </div>
+  );
 }
 
 export default AppLayout;
