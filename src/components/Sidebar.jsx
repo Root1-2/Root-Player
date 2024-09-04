0cc84cb1efbebf7826f329f3a94e47d059aa5d29
John Doe <abdullahalmasrur8@gmail.com>
2024-09-04 23:46:47 +0600

    static sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { GoGear, GoHome } from "react-icons/go";
 import { LuChevronFirst } from "react-icons/lu";
 
-function Sidebar() {
+export default function Sidebar({ children }) {
   return (
     <aside className="h-screen">
-      <nav className="flex h-full flex-col border-r bg-gray-400 shadow-sm">
+      <nav className="flex h-full flex-col border-r bg-gray-50 shadow-sm">
         <div className="item-center flex justify-between p-4 pb-2">
           <img src="" alt="Logo" className="w-32" />
           <button className="rounded-lg bg-gray-50 p-1.5 hover:bg-gray-100">
@@ -14,18 +14,10 @@ function Sidebar() {
           </button>
         </div>
 
-        <ul className="flex-1 px-3">
-          <li
-            className={`relative my-1 flex cursor-pointer items-center rounded-md px-3 py-2 font-medium transition-colors ${active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800" : "text-gray-600 hover:bg-indigo-50"}`}
-          >
-            <GoHome />
-            <span className="ml-3 w-52">Home</span>
-          </li>
-        </ul>
+        <ul className="flex-1 px-3">{children}</ul>
 
         <div className="flex border-t p-3">
           <button className="flex gap-2">
-            <GoGear />
             <span>Settings</span>
           </button>
         </div>
@@ -34,4 +26,16 @@ function Sidebar() {
   );
 }
 
-export default Sidebar;
+export function SidebarItem({ icon, text, active, alert }) {
+  return (
+    <li
+      className={`relative my-1 flex cursor-pointer items-center rounded-md px-3 py-2 font-medium transition-colors ${active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800" : "text-gray-600 hover:bg-indigo-50"}`}
+    >
+      {icon}
+      <span className="ml-3 w-52">{text}</span>
+      {alert && (
+        <div className={`absolute right-2 h-2 w-2 rounded bg-indigo-400`}></div>
+      )}
+    </li>
+  );
+}
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
 import React from "react";
+import { Outlet } from "react-router-dom";
 
-import Sidebar from "../components/Sidebar.jsx";
+import Sidebar, { SidebarItem } from "../components/Sidebar.jsx";
+import { GoHome, GoGear } from "react-icons/go";
 
 function AppLayout() {
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar>
+        <SidebarItem icon={<GoHome />} text="Home" active />
+        <SidebarItem icon={<GoGear />} text="Settings" />
+      </Sidebar>
+
       <div className="mx-5">
         <Outlet />
       </div>
