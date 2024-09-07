2e4c0c6ba3059856fc26b9155016d36bfe876e68
John Doe <abdullahalmasrur8@gmail.com>
2024-09-07 22:09:46 +0600

    #

--- src/components/Playbar.jsx
+++ src/components/Playbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Playbar() {
-  return <div className="">Footer</div>;
+  return <div className="">Playbar</div>;
+
+  // Play/Pause, Next, Previous, Volume, Equalizer, Queue, Playing Song Details
+  // Position will be at bottom
 }
 
 export default Playbar;
