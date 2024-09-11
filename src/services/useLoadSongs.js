45ec78f383f4d9a3359049650c2eaad0e61b4b1d
John Doe <abdullahalmasrur8@gmail.com>
2024-09-11 23:26:32 +0600

    #

--- src/services/useLoadSongs.js
+++ src/services/useLoadSongs.js
@@ -1,18 +1,18 @@
-import { useQuery } from 'react-query';
-import useLoadSongsFromDisk from './useLoadSongsFromDisk';
+import { useQuery } from "react-query";
+import useLoadSongsFromDisk from "./useLoadSongsFromDisk";
 
 const useLoadSongs = () => {
   const { directoryPath, loadSongs } = useLoadSongsFromDisk();
 
-  const { data: songs, isLoading, error } = useQuery(
-    ['songs', directoryPath],
-    () => loadSongs(directoryPath),
-    {
-      enabled: !!directoryPath,  // Only run when directoryPath is set
-      staleTime: Infinity,
-      cacheTime: Infinity,
-    }
-  );
+  const {
+    data: songs,
+    isLoading,
+    error,
+  } = useQuery(["songs", directoryPath], () => loadSongs(directoryPath), {
+    enabled: !!directoryPath,
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  });
 
   return { songs, isLoading, error, directoryPath };
 };
